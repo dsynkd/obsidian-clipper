b563be3a2f4b897257061fba79e0afd60c38c80e
Darren Sadr <darrensadr@gmail.com>
2025-08-07 12:01:30 -0700

    added npm run test

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,17 @@
 import type {Config} from 'jest';
 
+
 const config: Config = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
+	moduleNameMapper: {
+		'^utils/(.*)$': '<rootDir>/src/utils/$1',
+		'^managers/(.*)$': '<rootDir>/src/managers/$1',
+		'^icons$': '<rootDir>/src/icons',
+		'^core/(.*)$': '<rootDir>/src/core/$1',
+		'^types/(.*)$': '<rootDir>/src/types/$1',
+		'^styles/(.*)$': '<rootDir>/src/styles/$1',
+	},
 };
 
 export default config;
